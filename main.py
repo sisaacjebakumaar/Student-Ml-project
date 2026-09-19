9804e7d477f76711ef6f2719f465972d2226cc27
sisaacjebakumaar <carbun625@gmail.com>
2026-09-20 00:04:43 +0530

    nullset search

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
 import pandas as pd
 data = pd.read_csv("student_data.csv")
-print("First 5 rows of the dataset:")
-print(data.head())
-
-print("Dataset Information :")
-print(data.info())
-
-print("Summary Statistics :")
-print(data.describe())
+print("MISSING VALUES:")
+print(data.isnull().sum())
