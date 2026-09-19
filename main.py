6fe7104b0dfc52f3477c86df12554632ebf5a4e2
sisaacjebakumaar <carbun625@gmail.com>
2026-09-19 22:51:08 +0530

    v1

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 import pandas as pd
 data = pd.read_csv("student_data.csv")
-print(data)
+print("First 5 rows of the dataset:")
+print(data.head())
+
+print("Dataset Information :")
+print(data.info())
+
+print("Summary Statistics :")
+print(data.describe())
