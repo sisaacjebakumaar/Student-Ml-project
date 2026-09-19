becc2c403e31190022c4dad4cc9c0039425572cd
sisaacjebakumaar <carbun625@gmail.com>
2026-09-20 00:36:29 +0530

    new student

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ X = data[[
     "Study_Hours",
     "Attendance",
     "Previous_Mark",
-    "Final_Mark"
+    "Assignment"
 ]]
 
 y =data["Final_Mark"]
@@ -47,3 +47,15 @@ print("MSE:", mse)
 print("RMSE:", rmse)
 print("R2 Score:", r2)
 
+# New student data
+new_student = pd.DataFrame({
+    "Study_Hours": [6],
+    "Attendance": [85],
+    "Previous_Mark": [75],
+    "Assignment": [80]
+})
+
+# Predict final mark
+predicted_mark = model.predict(new_student)
+
+print("Predicted Final Mark:", predicted_mark[0])
