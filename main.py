d10f622470b3520625623b3d0706298210034203
sisaacjebakumaar <carbun625@gmail.com>
2026-09-20 00:22:37 +0530

    train_test_split

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
+
 data = pd.read_csv("student_data.csv")
 
 X = data[[
@@ -15,3 +17,17 @@ print(X)
 
 print("\nTarget:")
 print(y)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+print("\nTraining Features (X_train):")
+print(X_train)
+
+print("\nTesting Features (X_test):")
+print(X_test)
+
+print("\nTraining Target (y_train):")
+print(y_train)
+
+print("\nTesting Target (y_test):")
+print(y_test)
